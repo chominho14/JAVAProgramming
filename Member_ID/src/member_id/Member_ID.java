ca7ca0e97fa3045efb90facfd3e4f813e23fd768
chominho14 <chominho14@naver.com>
2021-09-23 21:28:39 +0900

    version01

---
+++ Member_ID/src/member_id/Member_ID.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package member_id;
+
+/**
+ *
+ * @author chominho
+ */
+public class Member_ID {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
